268a649c677f9a6c5dfb531620fad00388d7f322
Krithin Sitaram <me@krith.in>
2018-04-12 02:37:41 -0700

    Just store lat/longs when iterating through nodes instead of whole pyosmium node objects.
    This makes the iteration finish in under a minute instead of taking forever.
    
    Also get the output formatted right.

--- getlocations.py
+++ getlocations.py
@@ -5,37 +5,45 @@ import sys
 import random
 import osmium
 
+import common
+
 class RandomNodeSelector(osmium.SimpleHandler):
 	"""Picks 'node_count' uniformly randomly selected OSM nodes."""
 	def __init__(self, node_count: int) -> None:
 		osmium.SimpleHandler.__init__(self)
 		self.node_count = node_count
-		self.node_list: List[osmium.osm.Node] = []
+		self.location_list: List[common.Location] = []
 		self.node_index = 0  # indexes nodes while iterating over them
+
+	def _location(self, node: osmium.osm.Node) -> common.Location:
+		return common.Location(latitude = node.location.lat,
+		                       longitude = node.location.lon)
 
 	def node(self, n):
 		self.node_index += 1
-		if len(self.node_list) < self.node_count:
+		if self.node_index <= self.node_count:
 			# We're still looking at the initial few nodes
-			self.node_list.append(n)
+			self.location_list.append(self._location(n))
 		else:
 			# We have to pick the ith element with probability n/i
 			threshold = self.node_count / float(self.node_index)
 			if random.random() < threshold:
 				# We do want this element. Kick out one of the existing
 				# ones at random:
-				self.node_list[random.randrange(self.node_count)] = n
+				evicted_index = random.randrange(self.node_count)
+				self.location_list[evicted_index] = self._location(n)
 
 if __name__ == '__main__':
 	"""Picks out NUM_NODES randomly selected nodes from the given OSM
 	dataset and prints their lat,long coordinates.
 	"""
-	NUM_NODES = 10
+	NUM_NODES = 1000
 	if not len(sys.argv) == 2:
 		sys.exit('Usage: %s myregion.osm.pbf' % sys.argv[0])
 
 	random_handler = RandomNodeSelector(NUM_NODES)
 	random_handler.apply_file(sys.argv[1])
-	nodes = random_handler.node_list
+	locations = random_handler.location_list
 	
-	print(nodes)
+	for location in locations:
+		print('%f,%f' % (location.latitude, location.longitude))
